77519b0cddcf1a0f766b2d85793e634c4ada5efa
s1h1ikha <shikha11051996@gmail.com>
2017-04-18 19:43:21 +0530

    Update LuxuryGift.java

--- Q6/src/q6/LuxuryGift.java
+++ Q6/src/q6/LuxuryGift.java
@@ -12,12 +12,17 @@ package q6;
 public class LuxuryGift {
     double price, value;
 	String giftName;
-	int rating, difficulty;
+	
 	LuxuryGift(String arr[]) {
-                rating = Integer.parseInt(arr[0]);
-		difficulty = Integer.parseInt(arr[1]);
+              
 		value = Double.parseDouble(arr[2]);
 		price = Double.parseDouble(arr[3]);	
 		giftName = arr[4];
 	}
+	public boolean correct(int a,int e){
+            if(this.value>=e && this.price<=a)
+                    return true;
+            else
+                    return false;
+        }
 }
